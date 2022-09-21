8c4e0ad9a48a1688122f1f2bf7231cd391535fad
Denis_Gerasimov <Pythonchic@gmail.com>
2022-09-21 23:09:30 +0300

    initial commit

---
+++ Lesson01/Example003_/Program.cs
@@ -0,0 +1,9 @@
+﻿int numberA = new Random().Next(1, 10); // 1 2 3 4 ... 9
+int numberB = new Random().Next(1, 10);
+int result = numberA + numberB;
+Console.Write("Первое число: ");
+Console.WriteLine(numberA);
+Console.Write("Второе число: ");
+Console.WriteLine(numberB);
+Console.Write("Сумма этих чисел равна: ");
+Console.WriteLine(result);
---
+++ Lesson01/Example005_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "лариса")
+{
+        Console.WriteLine("Ура, это же Лариса!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
