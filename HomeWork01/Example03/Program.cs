11b3fd3403de8bd608755dfe34737a689c6d4992
Denis_Gerasimov <Pythonchic@gmail.com>
2022-09-22 00:04:59 +0300

    add HomeWork.

---
+++ HomeWork01/Example03/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите число для его проверки: ");
+string n = Console.ReadLine();
+int a = n;
+if(a % 2 == 0 )
+{
+    Console.Write("Число ");
+    Console.Write(n);
+    Console.WriteLine(" четное.");
+}
+else
+{
+    Console.Write("Число ");
+    Console.Write(n);
+    Console.WriteLine(" НЕчетное.");
+}
