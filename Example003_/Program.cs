2f54b42b6da671b682823de02c34913a37200ca4
Denis_Gerasimov <Pythonchic@gmail.com>
2022-09-21 21:48:34 +0300

    add random in Example 003 sum

--- Example003_/Program.cs
+++ Example003_/Program.cs
@@ -1,4 +1,9 @@
-﻿int numberA = 3;
-int numberB = 5;
+﻿int numberA = new Random().Next(1, 10); // 1 2 3 4 ... 9
+int numberB = new Random().Next(1, 10);
 int result = numberA + numberB;
+Console.Write("Первое число: ");
+Console.WriteLine(numberA);
+Console.Write("Второе число: ");
+Console.WriteLine(numberB);
+Console.Write("Сумма этих чисел равна: ");
 Console.WriteLine(result);
