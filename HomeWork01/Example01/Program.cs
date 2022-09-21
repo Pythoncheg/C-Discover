b4a9012ea88d848660c12129ad32e708f7d2eba2
Denis_Gerasimov <Pythonchic@gmail.com>
2022-09-22 00:20:15 +0300

    Выполнил задание Example01.

--- HomeWork01/Example01/Program.cs
+++ HomeWork01/Example01/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a = 2;
+int b = 10;
+int max = 0;
+int min = 0;
+
+if(a > b)
+{
+    max = a;
+    min = b;
+}
+else
+{
+    max = b;
+    min = a;
+}
+Console.Write("Максимальное число:");
+Console.WriteLine(max);
+Console.Write("Минимальное число:");
+Console.WriteLine(min);
