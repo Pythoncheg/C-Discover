ded3768fb0ebad30208d3e01af491644f8daa762
Denis_Gerasimov <Pythonchic@gmail.com>
2022-09-26 11:14:11 +0300

    Add HomeWork/Example04

---
+++ HomeWork01/Example04/Program.cs
@@ -0,0 +1,10 @@
+﻿int even = 2;
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+
+while(even <= n)
+{
+    Console.Write(even + " ");
+    even += 2;
+}
+Console.WriteLine("End");
