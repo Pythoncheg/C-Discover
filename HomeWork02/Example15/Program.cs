20017f1984f423253ba1af1595a2c061d89f6370
Denis_Gerasimov <Pythonchic@gmail.com>
2022-09-26 15:49:28 +0300

    Закончил задание 15

--- HomeWork02/Example15/Program.cs
+++ HomeWork02/Example15/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a = 5;
+if(a > 7)
+{
+    Console.WriteLine("Не корректное значение!");
+}
+else{
+    if(a == 6)
+    {
+        Console.WriteLine("Да.");
+    }
+    if(a == 7)
+        {
+            Console.WriteLine("Да.");
+        }
+        else
+        {
+        Console.WriteLine("Нет.");
+        }
+}    
