09e8e3ee5b90e5f3d91bc644251fe4199488714d
Denis_Gerasimov <Pythonchic@gmail.com>
2022-09-26 15:05:03 +0300

    Выполнил Example 228

--- HomeWork02/Example13/Program.cs
+++ HomeWork02/Example13/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int n = 325;
+int a = n % 100;
+int b = a % 10;
+int c = n / 100;
+int i = 0;
+
+Console.WriteLine(a);
+Console.WriteLine(b);
+Console.WriteLine(c);
+// while(i < 10)
+// {
+//     if(a == i)
+//     {
+//     Console.WriteLine($"Искомое число: {a}");
+//     }
+//     i++;
+//     Console.WriteLine("Нет такого числа.");
+// }
---
+++ HomeWork02/Example228/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+int[] n = new int[] {1, 2, 3, 4, 5, 6, 7};
+int sum = 0;
+int i = 0;
+int size = n.Length;
+
+while(i < size)
+{
+    sum = n[i] + sum;
+    i++;
+}
+int avg = sum / size;
+Console.WriteLine($"Общее арифметическое заданных чисел будет {avg}.");
