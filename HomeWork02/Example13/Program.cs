de83e8424d8433206e843d601fede70d16fd5417
Denis_Gerasimov <Pythonchic@gmail.com>
2022-09-26 16:55:58 +0300

    Решил задание 13

--- HomeWork02/Example13/Program.cs
+++ HomeWork02/Example13/Program.cs
@@ -1,18 +1,31 @@
-﻿int n = 325;
-int a = n % 100;
-int b = a % 10;
-int c = n / 100;
-int i = 0;
+﻿
 
-Console.WriteLine(a);
-Console.WriteLine(b);
-Console.WriteLine(c);
-// while(i < 10)
-// {
-//     if(a == i)
-//     {
-//     Console.WriteLine($"Искомое число: {a}");
-//     }
-//     i++;
-//     Console.WriteLine("Нет такого числа.");
-// }
+// Напишите программу, которая выводит третью цифру заданного числа или сообщает,
+//  что третьей цифры нет.
+
+// 645 -> 5
+
+// 78 -> третьей цифры нет
+
+// 32679 -> 6
+
+
+int n = 3345678;
+Console.WriteLine($"Задано число - {n}.");
+if(n < 99)
+{
+    Console.WriteLine("Нет третьей цифры!");
+}
+else
+{
+    while(n > 999)
+    {
+        n = n / 100;
+    }
+
+    n = n % 10;
+    Console.WriteLine($"Третья цифра в данном числе - {n}.");
+}
+
+
+
