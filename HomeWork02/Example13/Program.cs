50b3811163fef7f3eadb2ed39d3474512842bad5
Denis_Gerasimov <Pythonchic@gmail.com>
2022-09-29 20:04:24 +0300

    Доработал HomeWork02/Example 13

--- HomeWork02/Example13/Program.cs
+++ HomeWork02/Example13/Program.cs
@@ -11,21 +11,21 @@
 
 Console.Clear();
 Console.Write("Введите число: ");
-int n = int.Parse(Console.ReadLine());
-Console.WriteLine($"Задано число - {n}.");
-if(n < 99)
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine($"Задано число - {number}.");
+if(number < 99)
 {
     Console.WriteLine("Нет третьей цифры!");
 }
 else
 {
-    while(n > 999)
+    while(number > 999)
     {
-        n = n / 100;
+        number = number / 10;
     }
 
-    n = n % 10;
-    Console.WriteLine($"Третья цифра в данном числе - {n}.");
+    number = number % 10;
+    Console.WriteLine($"Третья цифра в данном числе - {number}.");
 }
 
 
---
+++ HomeWork03/Example19/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите метод, который принимает на вход пятизначное число и проверяет,
+//  является ли оно палиндромом.
+
+// 14212 -> нет
+
+// 12821 -> да
+
+// 23432 -> да
+int[] numbers = {1, 2, 3, 2, 1};
+int size = numbers.Length;
+
+void Method(int[] numbers)
+{
+    for(i = 0; i < size / 2; i++);
+    {
+        if (numbers[i] == numbers[size - 1 - i]);
+    }
+    Console.WriteLine("Палиндром.");
+}
