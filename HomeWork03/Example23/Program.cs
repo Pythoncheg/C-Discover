887aae8db14d3eb85744e056ac1e6c6aed430e39
Denis_Gerasimov <Pythonchic@gmail.com>
2022-10-03 14:16:47 +0300

    Выполнил Example 23

---
+++ HomeWork03/Example23/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите метод, который принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+
+void Method(int n) 
+{
+    double sum = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        sum = Math.Pow(i, 3); 
+        Console.Write($"{sum}, ");
+    }
+}
+Console.Clear();
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+Method(number);
