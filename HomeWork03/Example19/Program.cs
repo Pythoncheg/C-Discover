2238cf4709c514280cc0963c22861d813c7810cb
Denis_Gerasimov <Pythonchic@gmail.com>
2022-10-04 17:26:14 +0300

    Lesson4 initial commit

--- HomeWork03/Example19/Program.cs
+++ HomeWork03/Example19/Program.cs
@@ -6,55 +6,82 @@
 // 12821 -> да
 
 // 23432 -> да
-#region
+// #region
 
-int[] CreateArray(int size)
-{
-    return new int[size];
-}
+// int[] CreateArray(int size)
+// {
+//     return new int[size];
+// }
 
-void Fill(int[] array)
+// void Fill(int[] array)
+// {
+//     Console.WriteLine("Введите последовательно пять чисел от 1 до 9 для проверки на полиндром: ");
+//     int size = array.Length;
+//     for (int i = 0; i < size; i++)
+//     {
+//         array[i] = int.Parse(Console.ReadLine());
+//         if (array[i] > 9)
+//         {
+//             Console.WriteLine("Введено не корректное число!");
+//             i--;
+//         }
+//     }
+// }
+// void Print(int[] array)
+// {
+//     int size = array.Length;
+//     for (int i = 0; i < size; i++)
+//     {
+//         Console.Write(array[i] + " ");
+//     }
+//     Console.WriteLine();
+// }
+
+// void Palindrom(int[] array)
+// {
+//     int size = 5;
+//     int i = 0;
+//     if (array[i] == array[size - i - 1] && array[i + 1] == array[size - i - 2])
+//     {
+//         Console.WriteLine("Палиндром.");
+//     }
+//     else
+//     {
+//         Console.WriteLine("Не палиндром.");
+//     }
+// }
+// #endregion
+
+// Console.Clear();
+// int length = 5;
+// int[] numbers = CreateArray(length);
+// Fill(numbers);
+// Print(numbers);
+// Palindrom(numbers);
+void PalindromCheck(int numbers)
 {
-    Console.WriteLine("Введите последовательно пять чисел от 1 до 9 для проверки на полиндром: ");
-    int size = array.Length;
-    for (int i = 0; i < size; i++)
+    // int numbers = 45654;
+    int firsrNum = numbers / 10000;
+    int lastNum = numbers % 10;
+    int firstSecondNum = numbers % 10000;
+    int lastSecondNum = numbers % 100;
+    if (firstSecondNum > 9)
     {
-        array[i] = int.Parse(Console.ReadLine());
-        if (array[i] > 9)
-        {
-            Console.WriteLine("Введено не корректное число!");
-            i--;
-        }
+        firstSecondNum /= 1000;
     }
-}
-void Print(int[] array)
-{
-    int size = array.Length;
-    for (int i = 0; i < size; i++)
+    if (lastSecondNum > 9)
     {
-        Console.Write(array[i] + " ");
+        lastSecondNum /= 10;
     }
-    Console.WriteLine();
-}
-
-void Palindrom(int[] array)
-{
-    int size = 5;
-    int i = 0;
-    if (array[i] == array[size - i - 1] && array[i + 1] == array[size - i - 2])
+    if (firsrNum == lastNum && firstSecondNum == lastSecondNum)
     {
-        Console.WriteLine("Палиндром.");
+        Console.WriteLine("Палиндром");
     }
     else
     {
-        Console.WriteLine("Не палиндром.");
+        Console.WriteLine("Не палиндром");
     }
 }
-#endregion
 
-Console.Clear();
-int length = 5;
-int[] numbers = CreateArray(length);
-Fill(numbers);
-Print(numbers);
-Palindrom(numbers);
+int num = 12322;
+PalindromCheck(num);
