ea30a7dec41ceded3ea6db411f546953bf8e9a6b
Denis_Gerasimov <Pythonchic@gmail.com>
2022-10-10 17:30:23 +0300

    Complete Example 43

---
+++ HomeWork06/Example043/Program.cs
@@ -0,0 +1,35 @@
+﻿
+// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+// 1. Находим X и Y.
+// k1 * x + b1 = k2 * x + b2
+// x = (b2 - b1) / (k1 - k2)
+// y = k1 * x + b1
+
+// 2. Проверить разные ли точки.
+// 3. Добавить проверку пересекаются ли прямые.
+
+#region 
+Console.Write("Введите b1: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите k1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите b2: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите k2: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+double intersectionPointX = 0;
+double intersectionPointY = 0;
+#endregion
+
+if (b1 == b2 && k1 == k2) Console.WriteLine("Ваши точки совпадают, прямая ОДНА!");
+else if (k1 == k2) Console.WriteLine("Эти прямые не пересекаются!");
+else
+{
+    intersectionPointX = (b2 - b1) / (k1 - k2);
+    intersectionPointY = k1 * intersectionPointX + b1;
+    Console.WriteLine($"Точки пересечения прямых: ( {intersectionPointX} ; {intersectionPointY})");
+}
--- Seminar06/Example01/Program.cs
+++ Seminar06/Example01/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Составить частотный словарь элементов одномерного массива
+
+int [] arr = {};
+
+    // int count0 = 0;
+    // int count1 = 0;
+    // int count2 = 0;
+    // int count3 = 0;
+    // int count4 = 0;
+    // int count5 = 0;
+    // int count6 = 0;
+    // int count7 = 0;
+    // int count8 = 0;
+    // int count9 = 0;
+
+int frequencyArray(int [] array, int num)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i] == num) count++;
+    }
+    return count;
+}
+
+int countNum = frequencyArray(arr, 0);
+Console.WriteLine($"Число 0 встречается {countNum} раз");
+countNum = frequencyArray(arr, 1);
+Console.WriteLine($"Число 1 встречается {countNum} раз");
+countNum = frequencyArray(arr, 2);
+Console.WriteLine($"Число 2 встречается {countNum} раз");
+countNum = frequencyArray(arr, 3);
+Console.WriteLine($"Число 3 встречается {countNum} раз");
+countNum = frequencyArray(arr, 4);
+Console.WriteLine($"Число 4 встречается {countNum} раз");
+countNum = frequencyArray(arr, 5);
+Console.WriteLine($"Число 5 встречается {countNum} раз");
+countNum = frequencyArray(arr, 6);
+Console.WriteLine($"Число 6 встречается {countNum} раз");
+countNum = frequencyArray(arr, 7);
+Console.WriteLine($"Число 7 встречается {countNum} раз");
+countNum = frequencyArray(arr, 8);
+Console.WriteLine($"Число 8 встречается {countNum} раз");
+countNum = frequencyArray(arr, 9);
+Console.WriteLine($"Число 9 встречается {countNum} раз");
