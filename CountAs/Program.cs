48d93e688b97549cdef2a16f9badd3adeafcd5e5
Denis_Gerasimov <Pythonchic@gmail.com>
2022-10-11 12:25:53 +0300

    Initial commit Home work 7 Example 47

--- CountAs/Program.cs
+++ CountAs/Program.cs
@@ -3,21 +3,26 @@
 Console.Clear();
 Console.Write("Введите оценки: ");
 string m = Console.ReadLine();
-string[] arr = m.Split(" ");
+string[] arr = m.Split(' ', ',', '-');
 
-int[] ints = Array.ConvertAll(arr, s => int.Parse(s)); // Нашел этот метод в интернете.
+int[] estimates = Array.ConvertAll(arr, s => int.Parse(s)); // Нашел этот метод в интернете.
 int sum = 0;
-for (int i = 0; i < ints.Length; i++)
+for (int i = 0; i < estimates.Length; i++)
 {
-    if (ints[i] == 5) sum += 30;
-    else if (ints[i] == 4) sum += 15;
-    else if (ints[i] == 3) sum -= 15;
-    else if(ints[i] == 2) sum -= 30;
+    if (estimates[i] == 5) sum += 30;
+    else if (estimates[i] == 4) sum += 15;
+    else if (estimates[i] == 3) sum -= 15;
+    else if (estimates[i] == 2) sum -= 30;
 }
 
-Console.WriteLine(String.Join(',', ints));
-
-Console.Write($"В данный период ты заслужил {sum} рублей.");
+Console.WriteLine(String.Join(',', estimates));
+if (sum > 0) Console.WriteLine($"В данный период ты заслужил {sum} рублей.");
+else if(sum == 0)Console.WriteLine("Ты ничего не заработал, страйся лучше!");
+else
+{
+    int sums = Math.Abs(sum);
+    Console.WriteLine($"На данный момент ты должен отдать: {sums} рублей.");
+}
 Console.WriteLine();
 Console.Write("Нажмите Enter для выхода:");
-while (Console.ReadKey().Key != ConsoleKey.Enter) {}
+while (Console.ReadKey().Key != ConsoleKey.Enter) { }
