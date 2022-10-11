b10ca37407a657a271bfcf1dcf531f855c7a6559
Python <Pythonchic@gmail.com>
2022-10-11 20:43:25 +0300

    pull on notebook

--- HomeWork05/ExampleForOverGays/Program.cs
+++ HomeWork05/ExampleForOverGays/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу премножения любых целых чисел
+
+Console.Write("Введите первое число: ");
+SByte firstNumber = Convert.ToSByte(Console.ReadLine());
+Console.Write("Введите второе число: ");
+sbyte secondNumber = Convert.ToSByte(Console.ReadLine());
+sbyte sum = secondNumber + firstNumber;
+Console.WriteLine($"Сумма чисел равна: {sum}");
+sum.ToString.
