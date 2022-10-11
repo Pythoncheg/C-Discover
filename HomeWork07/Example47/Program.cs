a7398bcf30176ab49871ae71fd60dd12ed4ee88c
Denis_Gerasimov <Pythonchic@gmail.com>
2022-10-11 13:17:38 +0300

    Complete Example 47

--- HomeWork07/Example47/Program.cs
+++ HomeWork07/Example47/Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+// Задача 47. Задайте двумерный массив размером m×n,
+// заполненный случайными вещественными числами.
+
+// m = 3, n = 4.
+
+// 0,5 7 -2 -0,2
+
+// 1 -3,3 8 -9,9
+
+// 8 7,8 -7,1 9
+
+
+void PrintArray(double [,] matrix)
+{
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"[{matrix[i, j]}]");
+        }
+        Console.WriteLine();
+    }
+}
+
+void Fill(double [,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(-10, 10) / 10.0;
+        }
+    }
+}
+
+Console.WriteLine("Задайте двумерный массив размером MxN.");
+
+Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+double[,] numbArr =new double[m, n];
+Console.WriteLine();
+Fill(numbArr);
+PrintArray(numbArr);
+Console.WriteLine();
